5000bafb4c8202a1db9177ff3c4d48d77177b607
tavoTd <tavo_decadente14@hotmail.com>
2019-11-07 16:43:58 -0600

    Primer commit

---
+++ ProyectoConGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProyectoConGit
+//
+//  Created by CEDAM20 on 11/7/19.
+//  Copyright © 2019 CEDAM20. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
